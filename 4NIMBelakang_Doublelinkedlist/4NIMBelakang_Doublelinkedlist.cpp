132e469ce87fbaf76e205468aafe627a2f6e076c
Bangwahyu14 <whagung553@gmail.com>
2024-05-04 21:52:02 +0700

    membuat else provious

--- 4NIMBelakang_Doublelinkedlist/4NIMBelakang_Doublelinkedlist.cpp
+++ 4NIMBelakang_Doublelinkedlist/4NIMBelakang_Doublelinkedlist.cpp
@@ -103,6 +103,14 @@ void deleteNode()
 			START->prev = NULL;
 		}
 	}
+	else
+	{
+		previous->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = previous;
+		}
+	}
 
 
 
