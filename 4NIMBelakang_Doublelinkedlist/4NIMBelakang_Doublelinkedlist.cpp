9de8d5f6a9f06a6fe1d704e030bf835dbc50b908
Bangwahyu14 <whagung553@gmail.com>
2024-05-04 21:13:04 +0700

    menambahkan return newnode

--- 4NIMBelakang_Doublelinkedlist/4NIMBelakang_Doublelinkedlist.cpp
+++ 4NIMBelakang_Doublelinkedlist/4NIMBelakang_Doublelinkedlist.cpp
@@ -22,4 +22,10 @@ void addNode() {
 	if (START == NULL || newNode->noMhs <= START->noMhs) {
 
 		if (START != NULL && newNode->noMhs == START->noMhs) {
-			cout << "\033[31mDuplicate roll numbers not allowed
+			cout << "\033[31mDuplicate roll numbers not allowed \033[0m" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL) {
+			START->prev = newNode;
+
