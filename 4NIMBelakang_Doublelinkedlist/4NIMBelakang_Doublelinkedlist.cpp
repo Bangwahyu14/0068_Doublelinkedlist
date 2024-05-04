e19aa462c6a61c7903514f2dea5102de686eccb1
Bangwahyu14 <whagung553@gmail.com>
2024-05-04 21:10:21 +0700

    menambahkan if start

--- 4NIMBelakang_Doublelinkedlist/4NIMBelakang_Doublelinkedlist.cpp
+++ 4NIMBelakang_Doublelinkedlist/4NIMBelakang_Doublelinkedlist.cpp
@@ -18,3 +18,8 @@ void addNode() {
 	cin >> newNode->noMhs;
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name;
+
+	if (START == NULL || newNode->noMhs <= START->noMhs) {
+
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31mDuplicate roll numbers not allowed
